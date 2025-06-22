20b2c545a637c2360289d00270109f43baea1adf
Samsaw17 <cuervosamuel6@gmail.com>
2025-06-21 20:14:50 -0500

    Delete Moneda.py

--- Moneda.py
+++
@@ -1,8 +0,0 @@
-class Moneda:
-    def __init__(self, x, y):
-        self.posicionX = x
-        self.posicionY = y
-        self.activa = True
-        self.image_key = "moneda"
-        self.ancho = 30
-        self.alto = 30
